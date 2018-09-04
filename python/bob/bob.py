87cab0a549570693c99d70cbfc00a7f09eddc40a
Brian Mickel <brianemickel@berkeley.edu>
2018-09-04 10:48:36 -0700

    refactoring according to comments

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,27 +1,24 @@
 def hey(phrase):
+    phrase = phrase.strip()
     if is_addressed_nothing_said(phrase):
         return 'Fine. Be that way!'
-    elif is_question(phrase) and is_yell(phrase):
-        return 'Calm down, I know what I\'m doing!'
     elif is_question(phrase):
-        return 'Sure.'
+        if is_yell(phrase):
+            return 'Calm down, I know what I\'m doing!'
+        else:
+            return 'Sure.'
     elif is_yell(phrase):
         return 'Whoa, chill out!'
     else:
         return 'Whatever.'
 
 def is_question(phrase):
-    return phrase.rstrip()[-1] == "?"
+    return phrase.endswith("?")
 
 def is_yell(phrase):
-    return contains_letters(phrase) and (phrase.upper() == phrase)
+    return phrase.isupper()
 
 def is_addressed_nothing_said(phrase):
-    return "" == phrase.strip()
+    return not phrase
 
-def contains_letters(phrase):
-    return any(c.isalpha() for c in phrase)
-
-def contains_alphanumerics(phrase):
-    return any(c.isalnum() for c in phrase)
 
