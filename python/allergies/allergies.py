7d75a2aeea802637dbb89f6166a1ff1b0968d4d1
Brian Mickel <brianemickel@berkeley.edu>
2018-09-06 16:17:09 -0700

    moving allergies to enum.Flag only works in python3, bool() the is_allergic_to method

--- python/allergies/allergies.py
+++ python/allergies/allergies.py
@@ -1,30 +1,23 @@
+import enum
+
 class Allergies(object):
 
     def __init__(self, score):
         self._score = score
-        self._allergens = [allergen for idx, allergen in enumerate(potential_allergens) if (score&potential_allergens_bitflags[allergen])>>idx]
 
     def is_allergic_to(self, item):
-        return item in self.lst
+        return bool(self._score & Allergens[item].value)
 
     @property
     def lst(self):
-        return self._allergens
+        return [allergen.name for allergen in Allergens if (self._score & allergen.value)]
 
-potential_allergens_bitflags = {"eggs": 0b00000001,
-                        "peanuts": 0b00000010,
-                        "shellfish": 0b00000100,
-                        "strawberries": 0b00001000,
-                        "tomatoes": 0b00010000,
-                        "chocolate": 0b00100000,
-                        "pollen": 0b01000000,
-                        "cats": 0b10000000}
-potential_allergens = ["eggs",
-                        "peanuts",
-                        "shellfish",
-                        "strawberries",
-                        "tomatoes",
-                        "chocolate",
-                        "pollen",
-                        "cats"]
-        
+class Allergens(enum.Flag):
+    eggs = 0b00000001
+    peanuts = 0b00000010
+    shellfish = 0b00000100
+    strawberries = 0b00001000
+    tomatoes = 0b00010000
+    chocolate = 0b00100000
+    pollen = 0b01000000
+    cats = 0b10000000
