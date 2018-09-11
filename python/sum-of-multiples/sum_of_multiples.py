7df26657d673f208356dac4adf0246d003905488
Brian Mickel <brianemickel@berkeley.edu>
2018-09-11 01:50:06 -0700

    ready to submit sum of multiple v1

--- python/sum-of-multiples/sum_of_multiples.py
+++ python/sum-of-multiples/sum_of_multiples.py
@@ -1,26 +1,9 @@
-# def sum_of_multiples(limit, multiples):
-#    return sum([x for x in range(0,limit) if divisible_by(x, multiples)])
-
-# def divisible_by(x, multiples):
-#     for m in multiples:
-#         if x%m==0:
-#             return True
-#     return False
-
-
 def sum_of_multiples(limit, multiples):
-    overall_sum = 0
-    #generative
-    multiples_tuples = {}
+    multiplied_values = set()
     for m in multiples:
-        multiples_tuples[m] = int((limit-1)/m)
-    print(multiples_tuples)
-    multiplied = []
-    for m in multiples:
-        for i in range(1,multiples_tuples[m]+1):
-            sum_value = i*m
-            print(m,i,sum_value)
-            if sum_value not in multiplied:
-                overall_sum += sum_value
-            multiplied.append(sum_value)
+        max_val_coeff = int((limit-1)/m)
+        for i in range(1,max_val_coeff+1):
+            multiple_value = i*m
+            multiplied_values.add(multiple_value)
+    overall_sum = sum(multiplied_values)
     return overall_sum
