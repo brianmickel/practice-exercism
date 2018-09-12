3201b0063235863b544c49fe84589644715de090
Brian Mickel <brianemickel@berkeley.edu>
2018-09-12 04:31:20 -0700

    range returns an iterable and set().update() takes an iterable

--- python/sum-of-multiples/sum_of_multiples.py
+++ python/sum-of-multiples/sum_of_multiples.py
@@ -1,9 +1,6 @@
 def sum_of_multiples(limit, multiples):
     multiplied_values = set()
     for m in multiples:
-        max_val_coeff = int((limit-1)/m)
-        for i in range(1,max_val_coeff+1):
-            multiple_value = i*m
-            multiplied_values.add(multiple_value)
+        multiplied_values.update(range(0,limit,m))
     overall_sum = sum(multiplied_values)
     return overall_sum
