d3ef2ea18181eca193d234e48f6fb0574a522ad6
Brian Mickel <brianemickel@berkeley.edu>
2018-09-15 16:21:11 -0700

    finished sum of multiples

--- python/sum-of-multiples/sum_of_multiples.py
+++ python/sum-of-multiples/sum_of_multiples.py
@@ -1,6 +1,5 @@
 def sum_of_multiples(limit, multiples):
     multiplied_values = set()
     for m in multiples:
-        multiplied_values.update(range(0,limit,m))
-    overall_sum = sum(multiplied_values)
-    return overall_sum
+        multiplied_values.update(range(m,limit,m))
+    return sum(multiplied_values)
