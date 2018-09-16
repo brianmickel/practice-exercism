64f31674efe54f9f4ef5ebab9099a51a7353a3ea
Brian Mickel <brianemickel@berkeley.edu>
2018-09-16 15:30:07 -0700

    working to break down kindergarten into methods

--- python/kindergarten-garden/kindergarten_garden.py
+++ python/kindergarten-garden/kindergarten_garden.py
@@ -7,18 +7,26 @@ class Garden(object):
         self._students = sorted(students)
     
     def plants(self, student_name):
-        student_position = self._students.index(student_name)
-        plant_positions = [student_position*2,student_position*2+1]
+        plant_positions = self.getPlantPositionsFromStudentName(student_name)
         plant_rows = self._diagram.split('\n')
-        letters = []
-        for r in plant_rows:
-            for p in plant_positions:
-                letters.append(r[p])
+        letters = self.getPlantLettersFromRows(plant_rows, plant_positions)
         student_plants = self.getPlantNamesFromLetters(letters)
         return student_plants
 
+    def getPlantPositionsFromStudentName(self, student_name):
+        student_position = self._students.index(student_name)
+        return [student_position*2,student_position*2+1]
+
+    def getPlantLettersFromRows(self, rows, positions):
+        letters = []
+        for r in rows:
+            for p in positions:
+                letters.append(r[p])
+        return letters
+        
     def getPlantNamesFromLetters(self, letters):
         return [Plants[l].value for l in letters]
+    
 
 class Plants(enum.Enum):
     V = 'Violets'
