9560b545c55c89b1d9fe2e29fa584f30137eb4e0
Brian Mickel <brianemickel@berkeley.edu>
2018-09-22 18:30:34 -0700

    fixing the ugly methods with list comprehension and slicing

--- python/kindergarten-garden/kindergarten_garden.py
+++ python/kindergarten-garden/kindergarten_garden.py
@@ -3,30 +3,14 @@ class Garden(object):
     def __init__(self, diagram, students = ['Alice', 'Bob', 'Charlie', 'David',
                                             'Eve', 'Fred', 'Ginny', 'Harriet',
                                             'Ileana', 'Joseph', 'Kincaid', 'Larry']):
-        self._diagram = diagram
-        self._students = sorted(students)
+        self.diagram = diagram.splitlines()
+        self.students = sorted(students)
     
     def plants(self, student_name):
-        plant_positions = self.getPlantPositionsFromStudentName(student_name)
-        plant_rows = self._diagram.split('\n')
-        letters = self.getPlantLettersFromRows(plant_rows, plant_positions)
-        student_plants = self.getPlantNamesFromLetters(letters)
-        return student_plants
-
-    def getPlantPositionsFromStudentName(self, student_name):
-        student_position = self._students.index(student_name)
-        return [student_position*2,student_position*2+1]
-
-    def getPlantLettersFromRows(self, rows, positions):
-        letters = []
-        for r in rows:
-            for p in positions:
-                letters.append(r[p])
-        return letters
-        
-    def getPlantNamesFromLetters(self, letters):
-        return [Plants[l].value for l in letters]
-    
+        p = self.students.index(student_name)*2
+        letters = "".join([row[p:p+2] for row in self.diagram])
+        student_plants = [Plants[l].value for l in letters]
+        return student_plants   
 
 class Plants(enum.Enum):
     V = 'Violets'
